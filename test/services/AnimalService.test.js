a85b736dbd20221c4725a954ccfb657b6fc385fb
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-15 14:02:20 -0500

    Se creo service.js

--- test/services/AnimalService.test.js
+++ test/services/AnimalService.test.js
@@ -4,6 +4,6 @@ describe("Pruebas en AnimalServices", () =>{
     test("1. prueba de información de animales", () => {
         const dbAnimal = {id: 1, name: "tortuga"};
         const allAnimals = AnimalService.allAnimals(dbAnimal);
-        expect(allAnimals.name).toBe("tortua");
+        expect(allAnimals.name).toBe("tortuga");
     });
 })
