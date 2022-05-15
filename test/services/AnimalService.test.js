148de59f6d8468639bf86a60be68874556020dc1
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-15 17:39:49 -0500

    se aprovo segunda prueba de unidad

--- test/services/AnimalService.test.js
+++ test/services/AnimalService.test.js
@@ -1,7 +1,7 @@
 const AnimalService = require("../../lib/services/AnimalService");
 
 describe("Pruebas en AnimalServices", () =>{
-    test("1. prueba de información de animales", () => {
+    test("1. Prueba de información de animales", () => {
         const dbAnimal = {id: 1, name: "tortuga"};
         const allAnimals = AnimalService.allAnimals(dbAnimal);
         expect(allAnimals.name).toBe("tortuga");
