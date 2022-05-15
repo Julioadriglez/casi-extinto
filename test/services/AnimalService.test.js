3ce8865d86fe42ed1b338423ba56ec789fc0cefd
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-15 15:48:11 -0500

    se agrego metodo animalFilter

--- test/services/AnimalService.test.js
+++ test/services/AnimalService.test.js
@@ -8,7 +8,7 @@ describe("Pruebas en AnimalServices", () =>{
     });
     test("2. Filtro de animales por name ", () =>{
         const dbAnimal = [{id: 1, name: "tortuga"},{id: 2, name: "tortuga cahuamera"}, {id: 3, name: "foca"}];
-        const allAnimals = AnimalService.animalsFilter(dbAnimal,"tortuga")
+        const allAnimals = AnimalService.animalFilter(dbAnimal,"tortuga")
         expect(allAnimals.name).toBe("tortuga")
     })
 })
