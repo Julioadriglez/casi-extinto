3982d6204df0aebd5756ea7278081a30526acbf9
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-15 17:51:30 -0500

    Se realizo prueba a Reader y paso

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("../../lib/utils/Reader")
+
+describe("Prueba de Reader", () => {
+    test("1. prueba de reader que regrese información", () => {
+        const allAnimals = Reader.readJsonFile("dbAnimals.json");
+        expect(allAnimals).not.toBeUndefined();
+    });
+});
